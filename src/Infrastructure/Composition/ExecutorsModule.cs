1c93cbb986686e403bf5d43101775322daf266d0
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-12-04 23:00:00 +0100

    added ExecutorsModule - registering all interface implementations

---
+++ src/Infrastructure/Composition/ExecutorsModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using Procent.Demo.AutofacModules.Core.Execution;
+
+namespace Procent.Demo.AutofacModules.Infrastructure.Composition
+{
+    public class ExecutorsModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterAssemblyTypes(typeof (IExecuteAction).Assembly)
+                .Where(x => typeof (IExecuteAction).IsAssignableFrom(x))
+                .AsImplementedInterfaces();
+        }
+    }
+}
