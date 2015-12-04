7c9a322ee908366ca8453bcf91d8746d77b58e9d
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-12-04 23:00:00 +0100

    added logic to Main() - executing all actions

--- src/CmdlineApp/Program.cs
+++ src/CmdlineApp/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Procent.Demo.AutofacModules
+﻿using System.Collections.Generic;
+using Autofac;
+using Procent.Demo.AutofacModules.Core.Execution;
+using Procent.Demo.AutofacModules.Infrastructure;
+
+namespace Procent.Demo.AutofacModules
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyModules(typeof (IDetermineInfrastructureAssembly).Assembly);
+            using (IContainer container = builder.Build())
+            {
+                using (var scope = container.BeginLifetimeScope())
+                {
+                    var executors = scope.Resolve<IEnumerable<IExecuteAction>>();
+
+                    foreach (var executor in executors)
+                    {
+                        executor.Execute();
+                    }
+                }
+            }
         }
     }
 }
