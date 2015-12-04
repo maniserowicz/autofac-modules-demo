612c8e3c23f6b6221aa6047a5181eacec0634fd1
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-12-04 23:00:00 +0100

    added task for sending emails

---
+++ src/Core/Execution/SendEmails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Procent.Demo.AutofacModules.Core.Execution
+{
+    public class SendEmails : IExecuteAction
+    {
+        private readonly EmailConfiguration _configuration;
+
+        public SendEmails(EmailConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Sending emails from {0}", _configuration.FromAddress);
+        }
+    }
+
+    public class EmailConfiguration
+    {
+        public string FromAddress { get; private set; }
+
+        public EmailConfiguration(string fromAddress)
+        {
+            FromAddress = fromAddress;
+        }
+    }
+}
