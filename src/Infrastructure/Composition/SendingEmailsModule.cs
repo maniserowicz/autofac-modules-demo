33453ac502f3ee7eef1be61b3def4a8649fcc3a4
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-12-04 23:00:00 +0100

    added module with email configuration registration

---
+++ src/Infrastructure/Composition/SendingEmailsModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using Autofac;
+using Procent.Demo.AutofacModules.Core.Execution;
+
+namespace Procent.Demo.AutofacModules.Infrastructure.Composition
+{
+    public class SendingEmailsModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            var email_config = new EmailConfiguration(ConfigurationManager.AppSettings["email-from"]);
+            builder.RegisterInstance(email_config)
+                .SingleInstance();
+        }
+    }
+}
