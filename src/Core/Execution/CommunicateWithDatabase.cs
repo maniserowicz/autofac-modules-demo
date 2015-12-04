dfaa2181ddec67b0131eb24ab5078c96ee0182f3
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2015-12-04 23:00:00 +0100

    added task for communication with DBs

---
+++ src/Core/Execution/CommunicateWithDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Procent.Demo.AutofacModules.Core.Execution
+{
+    public class CommunicateWithDatabase : IExecuteAction
+    {
+        private readonly Func<DatabaseType, string> _connectionStringFactory;
+
+        public CommunicateWithDatabase(Func<DatabaseType, string> connectionStringFactory)
+        {
+            _connectionStringFactory = connectionStringFactory;
+        }
+
+        public void Execute()
+        {
+            communicate(DatabaseType.Own);
+            communicate(DatabaseType.External);
+        }
+
+        void communicate(DatabaseType databaseType)
+        {
+            string connection_string = _connectionStringFactory(databaseType);
+            Console.WriteLine("communicating with DB {0}: {1}", databaseType, connection_string);
+        }
+    }
+
+    public enum DatabaseType
+    {
+        Own,
+        External
+    }
+}
